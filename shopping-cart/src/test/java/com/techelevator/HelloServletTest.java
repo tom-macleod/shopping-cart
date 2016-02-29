56aa5ce748aee1c0dfe7b7ed7034a88dba2088b9
David Wintrich <david@techelevator.com>
2016-02-29 14:17:15 -0500

    skeleton project for Shopping Cart Exercise

---
+++ shopping-cart/src/test/java/com/techelevator/HelloServletTest.java
@@ -0,0 +1,12 @@
+package com.techelevator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloServletTest {
+	
+	@Test
+	public void a_silly_test_that_always_passes() {
+		Assert.assertEquals(2, 2);
+	}
+}
